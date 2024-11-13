9a9b2625c9ab6e86bd8ef5f06c0c65f6e942abe1
danielsilvalima1996 <dsl15021996@gmail.com>
2024-11-13 19:33:31 -0300

    ajustado http metodo

--- src/main/java/br/ce/wcaquino/taskbackend/controller/TaskController.java
+++ src/main/java/br/ce/wcaquino/taskbackend/controller/TaskController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,7 +45,7 @@ public class TaskController {
 		return new ResponseEntity<Task>(saved, HttpStatus.CREATED);
 	}
 
-	@PostMapping
+	@DeleteMapping
 	public ResponseEntity<Void> delete(@PathVariable(value = "id") Long id) throws ValidationException {
 		if(id == null) {
 			throw new ValidationException("Fill the task id");
