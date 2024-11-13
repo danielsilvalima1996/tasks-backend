6a5b00fa8d3d72edc74c1eb106ae87cc97dd5857
danielsilvalima1996 <dsl15021996@gmail.com>
2024-11-13 19:31:28 -0300

    adicionado metodo delete

--- src/main/java/br/ce/wcaquino/taskbackend/controller/TaskController.java
+++ src/main/java/br/ce/wcaquino/taskbackend/controller/TaskController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,5 +42,14 @@ public class TaskController {
 		}
 		Task saved = todoRepo.save(todo);
 		return new ResponseEntity<Task>(saved, HttpStatus.CREATED);
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> delete(@PathVariable(value = "id") Long id) throws ValidationException {
+		if(id == null) {
+			throw new ValidationException("Fill the task id");
+		}
+		todoRepo.deleteById(id);
+		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 	}
 }
